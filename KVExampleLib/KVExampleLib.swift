92a98b7324713d5f660cfacc161e6ec0728dc17c
KalaivaniVelusamy <capgemini.v.v@daimler.com>
2020-08-23 09:28:44 +0530

    added test cases

---
+++ KVExampleLib/KVExampleLib.swift
@@ -0,0 +1,21 @@
+//
+//  KVExampleLib.swift
+//  KVExampleLib
+//
+//  Created by V, Kalaivani V. (623-Extern) on 23/08/20.
+//  Copyright © 2020 Kalaivani. All rights reserved.
+//
+
+import Foundation
+
+public final class KVExampleLib{
+    
+    public func add(a: Int, b: Int) -> Int {
+           return a + b
+       }
+       
+       public func sub(a: Int, b: Int) -> Int {
+           return a - b
+       }
+    
+}
--- KVExampleLibTests/KVExampleLibTests.swift
+++ KVExampleLibTests/KVExampleLibTests.swift
@@ -8,9 +8,14 @@
 
 import XCTest
 
+@testable import KVExampleLib
+
 class KVExampleLibTests: XCTestCase {
+    
+    var kvexampleLib: KVExampleLib!
 
     override func setUpWithError() throws {
+        kvexampleLib = KVExampleLib()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
@@ -18,10 +23,15 @@ class KVExampleLibTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testAdd() throws {
+       XCTAssertEqual(kvexampleLib.add(a: 1, b: 1), 2)
     }
+    
+    func testSub() {
+        XCTAssertEqual(kvexampleLib.sub(a: 2, b: 1), 1)
+    }
+    
+    
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
