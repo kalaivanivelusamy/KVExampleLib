7ea3440ff4ef17f5bf6022e8d1c733e5a3284224
KalaivaniVelusamy <capgemini.v.v@daimler.com>
2020-08-23 08:24:01 +0530

    Initial project setup

---
+++ KVExampleLib/KVExampleLib.h
@@ -0,0 +1,19 @@
+//
+//  KVExampleLib.h
+//  KVExampleLib
+//
+//  Created by V, Kalaivani V. (623-Extern) on 23/08/20.
+//  Copyright © 2020 Kalaivani. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KVExampleLib.
+FOUNDATION_EXPORT double KVExampleLibVersionNumber;
+
+//! Project version string for KVExampleLib.
+FOUNDATION_EXPORT const unsigned char KVExampleLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KVExampleLib/PublicHeader.h>
+
+
